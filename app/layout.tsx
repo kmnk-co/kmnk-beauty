3ce6d27d9ca53e3a97273ea14ca13aadcaef3e90
Kali Kaminski <kali.kaminski93@icloud.com>
2025-03-18 08:00:48 -0500

    New footer broke homepage so I fixered it

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,20 +14,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="pt-10 pb-5">{children}</div>
-        <div className="py-5 text-center hind-light">
-          <div>
-            © 2025{" "}
-            <a href="https://kmnk.beauty/" className="hover:underline">
-              KMNK™ Beauty.
-            </a>{" "}
-            All rights reserved.
-          </div>
-          <div>
-            Created with love by{" "}
-            <a href="https://kmnk.co/" className="hover:underline">
-              KMNK™ Co.
-            </a>
+        <div className="flex flex-col min-h-full">
+          <div className="flex-grow pt-10 pb-5">{children}</div>
+          <div className="flex-grow-0 flex-shrink-0 py-5 text-center hind-light">
+            <div>
+              © 2025{" "}
+              <a href="https://kmnk.beauty/" className="hover:underline">
+                KMNK™ Beauty.
+              </a>{" "}
+              All rights reserved.
+            </div>
+            <div>
+              Created with love by{" "}
+              <a href="https://kmnk.co/" className="hover:underline">
+                KMNK™ Co.
+              </a>
+            </div>
           </div>
         </div>
       </body>
