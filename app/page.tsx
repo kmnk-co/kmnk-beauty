97295da126c8de27a3f98d259ac83e98a33fbce4
Kali Kaminski <kali.kaminski93@icloud.com>
2025-05-07 15:12:59 -0500

    Added max width to jumbotron

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,20 @@ import Image from "next/image";
 
 const Page = () => {
   return (
-    // <div className="h-[50vh] flex justify-center items-center">
-    //   <Image src={"globe.svg"} alt={"globe"} height={100} width={100} />
-    //   <div className="text-center">
-    //     <div className="text-[4.5rem] daniel-normal brand-gradient">
-    //       KMNK Beauty
-    //     </div>
-    //     <div className="text-xl hind-light">We&apos;ll be right back!</div>
-    //   </div>
-    // </div>
-    // <div className="relative h-auto w-full">
-    //   <Image src={"/stock-photo-hires.png"} alt={"a"} fill={true} objectFit="cover" />
-    // </div>
-    <div className="">
-      <Image
-      className="dark:hidden"
-        src={"/jumbotron.png"}
-        alt="Picture of the author"
-        sizes="100vw"
-        style={{
-          width: "100%",
-          height: "auto",
-        }}
-        width={16}
-        height={10}
-      />
+    <div className="bg-foreground">
+      <div className="mx-auto max-w-screen-xl">
+        <Image
+          src={"/jumbotron.png"}
+          alt="Picture of the author"
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          width={16}
+          height={9}
+        />
+      </div>
     </div>
   );
 };
