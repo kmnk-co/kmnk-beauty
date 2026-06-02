92cc9c31f8f3885b3396d93a563db7c19bfdcaf8
Kamella <75056083+KKaminski93@users.noreply.github.com>
2026-06-02 10:50:35 -0500

    Implement redirect for /book to external site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/book",
+        destination: "https://cal.com/kmnk-beauty",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
